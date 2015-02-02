2978347637474114fb3cb54175bd1dca58188e8d
AdamThePieRat <adamjc86@gmail.com>
2015-02-02 20:05:19 +0000

    Handling new todo items

--- src/test.js
+++ src/test.js
@@ -3,10 +3,16 @@ var AppDispatcher = new Flux.Dispatcher();
 var TodoStore = {
     todos: [],
 
+    updateTodo: function(todo) {
+        this.todos.splice(todo.id, 1, todo);
+
+        console.log(todo);
+    },
+
     getAll: function () {
         return this.todos;
     }
-}
+};
 
 MicroEvent.mixin(TodoStore);
 
@@ -14,6 +20,10 @@ AppDispatcher.register(function (data) {
     switch (data.eventName) {
         case 'new-todo':
             TodoStore.todos.push(data.newTodo);
+            TodoStore.trigger('change');
+            break;
+        case 'update-todo':
+            TodoStore.updateTodo(data.todo);
             TodoStore.trigger('change');
             break;
         case 'remove-todo':
@@ -25,4 +35,4 @@ AppDispatcher.register(function (data) {
     }
 
     return true;
-})
+});
